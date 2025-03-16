ec9dde21ff4df7d19f2a4a91e8a5d5eeb5ddbe78
raul far <raulfarllobell@gmail.com>
2025-03-16 22:10:44 +0100

    feat(head): add length en the card header

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,28 +1,39 @@
 "use client";
 
+import { useCartProduct } from "@/src/context/ProductContext";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { storage } = useCartProduct();
   return (
     <header className="w-full mb-5 h-24 flex justify-between items-center">
-      <span className="text-2xl font-semibold">
+      <Link
+        href={"/"}
+        className="w-fit hover:opacity-80 transition-opacity text-2xl font-semibold"
+      >
         <Image
           src={"/icons/logotype.svg"}
           alt="image reference logotype"
-          width={160}
-          height={160}
+          width={120}
+          height={120}
         />
-      </span>
-      <div className="flex items-center gap-2">
+      </Link>
+      <Link
+        href="/overview/cart"
+        className="p-2 rounded-full cursor-pointer hover:opacity-80 transition-all hover:bg-slate-100 justify-center flex items-center"
+      >
         <Image
           src={"/icons/cart.svg"}
           alt="image reference cart"
-          width={24}
-          height={24}
+          width={36}
+          height={36}
         />
-        <span className="font-light text-lg">0</span>
-      </div>
+        <span className="font-light text-2xl text-black -mb-2">
+          {storage.length ?? 0}
+        </span>
+      </Link>
     </header>
   );
 };
